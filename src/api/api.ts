fb29c1ae50af9375d806c28aadcfdc5ee44c0c03
thisWeb1225 <shan881225@gmail.com>
2023-03-21 00:09:35 +0800

    :white_check_mark: Feat: Add getAccessToken api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,27 @@
 import axios from "axios";
 
-const api = {
+interface ApiType {
+  getAccessToken: (codeParam: string) => any;
+}
 
+const api: ApiType = {
+  getAccessToken: async (codeParam) => {
+    try {
+      let response = await axios({
+        method: 'GET',
+        url: 'http://localhost:4000/getAccessToken',
+        params: {
+          code: codeParam,
+        }
+      })
+    
+      const data = await response.data;
+      return data;
+      
+    } catch (err) {
+      return err;
+    }
+  }
 }
 
 export default api;
