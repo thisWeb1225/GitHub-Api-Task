ca93d8decb6cb59a0fdcf3b50316c30bca9d409f
thisWeb1225 <shan881225@gmail.com>
2023-03-20 23:50:21 +0800

    :white_check_mark: Feat: Add codeParam effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const CLIENT_ID = '2042d1629d1bc7b1b64f';
 
 function App() {
   const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParam = new URLSearchParams(queryString);
+    const codeParam = urlParam.get('code');
+
+    console.log(codeParam);
+  })
 
   const loginWithGitHub = () => {
     window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
