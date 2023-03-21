396e4be59ec1edd1f869d2c1046e2cd6f24e1cc7
thisWeb1225 <shan881225@gmail.com>
2023-03-21 17:17:55 +0800

    :white_check_mark: Feat: Add getRepoIssues api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,25 +1,54 @@
 import axios from "axios";
 
 interface ApiType {
-  getAccessToken: (codeParam: string | null) => any;
+  getAccessToken: (codeParam: string | null) => Promise<any>;
+  getRepoIssues: (params: ParamsType) => Promise<any>
+}
+
+interface ParamsType {
+  token: string | null;
+  editedTitle?: string;
+  editedBody?: string;
+  id?: number;
 }
 
 const api: ApiType = {
   getAccessToken: async (codeParam) => {
     try {
-      let response = await axios({
+      const response: any = await axios({
         method: 'GET',
         url: 'http://localhost:4000/getAccessToken',
         params: {
           code: codeParam,
         }
       })
-      const data = await response.data;
-      return data;
+
+      if (response.state === 200) {
+        const data = await response.data;
+        return data;
+      } else {
+        throw new Error('state error')
+      }
+     
 
     } catch (err) {
-      return err;
+      console.error(err);
+      throw new Error('error');
     }
+  },
+
+  getRepoIssues: async (params) => {
+    const {token} = params;
+    const response = await axios({
+      method: 'GET',
+      url: 'http://localhost:4000/getRepoIssues',
+      params: {
+        token
+      }
+    })
+
+    const data = await response.data;
+    return data;
   }
 }
 
