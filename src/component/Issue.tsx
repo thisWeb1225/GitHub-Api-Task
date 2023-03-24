5605217e641aa8d5ed49b7e0ace53d2599a02385
thisWeb1225 <shan881225@gmail.com>
2023-03-24 17:45:06 +0800

    :white_check_mark: Feat: add issueModal component

--- src/component/Issue.tsx
+++ src/component/Issue.tsx
@@ -3,6 +3,9 @@ import { IssueType } from '../context/IssuesProvider';
 import { ReducerAction } from '../context/IssuesProvider';
 import { ReducerActionType } from '../context/IssuesProvider';
 
+import IssueModal from './IssueModal';
+import { useState } from 'react';
+
 
 type PropsType = {
   issue: IssueType,
@@ -12,13 +15,29 @@ type PropsType = {
 
 const Issue = ({ issue, dispatch, REDUCER_ACTIONS }: PropsType) => {
 
+  const [isModalShow, setIsModalShow] = useState<boolean>(false)
+
   const { title, body, state, number } = issue;
 
+  const openIssueModal = () => {
+    setIsModalShow(true)
+  }
+
   return (
-    <div>
-      <h2 className='issueCard__title'>{title}</h2>
-      <p className='issueCard__body'>{body}</p>
-    </div>
+    <>
+      <div className='issueCard' onClick={openIssueModal}>
+        <h2 className='issueCard__title'>{title}</h2>
+        <p className='issueCard__body'>{body}</p>
+      </div>
+      <IssueModal
+        issue={issue}
+        dispatch={dispatch}
+        REDUCER_ACTIONS={REDUCER_ACTIONS}
+        isModalShow={isModalShow}
+        setIsModalShow={setIsModalShow}
+      />
+    </>
+
   )
 }
 
---
+++ src/component/IssueModal.tsx
@@ -0,0 +1,61 @@
+import Modal from 'react-modal';
+import { useEffect, useState } from 'react';
+import { IssueType } from '../context/IssuesProvider';
+import { ReducerAction } from '../context/IssuesProvider';
+import { ReducerActionType } from '../context/IssuesProvider';
+
+Modal.setAppElement('#root');
+
+type PropsType = {
+  issue: IssueType;
+  dispatch: React.Dispatch<ReducerAction>,
+  REDUCER_ACTIONS: ReducerActionType
+  isModalShow: boolean;
+  setIsModalShow: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const IssueModal = ({ issue, dispatch, REDUCER_ACTIONS, isModalShow, setIsModalShow }: PropsType) => {
+
+  const [editedTitle, setEditedTitle] = useState('');
+  const [editedBody, setEditedBody] = useState('');
+
+  const { number, title, body, state } = issue
+
+  const closeIssueModal = () => {
+    setIsModalShow(() => false);
+  }
+
+  const saveIssueModal = () => {
+    // api
+  }
+
+  useEffect(() => {
+    if (isModalShow) {
+      setEditedTitle(title);
+      setEditedBody(body);
+    } else {
+      setEditedTitle('');
+      setEditedBody('');
+    }
+  }, [isModalShow])
+
+  return (
+    <Modal isOpen={isModalShow} onRequestClose={closeIssueModal}>
+      <h2>編輯 {title}</h2>
+      {/* <label className="modal__title">
+        標題：
+        <input className="modal__titleInput" type="text" value={editedTitle} onChange={(e) => setEditedTitle(e.target.value)} />
+      </label>
+      <label>
+        內容：
+        <textarea value={editedBody} onChange={(e) => setEditedBody(e.target.value)} />
+      </label> */}
+      <div>
+        <button onClick={saveIssueModal}>儲存</button>
+        <button onClick={closeIssueModal}>取消</button>
+      </div>
+    </Modal>
+  )
+}
+
+export default IssueModal;
