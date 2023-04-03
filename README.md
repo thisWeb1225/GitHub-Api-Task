8d000185dbddd8e07cb3d6b330456e159a7379ce
thisWeb1225 <shan881225@gmail.com>
2023-04-03 16:09:13 +0800

    :memo: Docs: Modify README.md, add gif to show feature

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # GitHub Issues API
 利用 GitHub OAuth 做第三方登陸，並用 GitHub API 對指定倉庫的 Issues 做增刪改查...等功能
+![imgae](src/assets/heroImg.gif)
 
 ## 啟動
 ```shell
@@ -10,14 +11,10 @@ $ node server.js
 ```
 
 # 使用技術
-1. vite + react
-   利用 Vite 快速搭建 React + TypeScript 應用程式
-2. typescript
-   使用 TypeScript 增加程式碼的可讀性、可維護性
-3. axios
-   使用 Axios 發送 HTTP 請求，並處理響應
-4. fortawesome
-   使用 fortawesome 的 svg 圖標
+1. vite + react: 利用 Vite 快速搭建 React + TypeScript 應用程式
+2. TypeScript: 使用 TS 增加程式碼的可讀性、可維護性
+3. axios: 使用 Axios 發送 HTTP 請求，並處理響應
+4. fortawesome: 使用 fortawesome 的 svg 圖標
 
 ## 利用 OAuth 做第三方登錄
 可用 GitHub 帳號登陸，登陸後即可對 issue 做曾刪改查。除此之外，利用 localStorage 來儲存資料，避免每次進入網站都需要重複登錄。
@@ -25,11 +22,21 @@ $ node server.js
 ## Issues API
 利用 github issue api 來對 issue 增刪改查，並在前端實現搜尋、分類、排序等功能。
 
+### 搜尋
+![search](src/assets/search.gif)
+
+### 分類、排序
+![sort](src/assets/sort.gif)
+
+## 新增、修改、刪除
+![add](src/assets/add.gif)
+
 ## Lazy loading
 使用 Intersection Observer 對 footer 進行觀察，當 footer 進入頁面後抓取下十筆資料，當回傳資料為空陣列後，取消觀察。 
 
 ## 前端頁面
-利用 CSS grid 製作前端頁面，並使用媒體查詢做響應式設計。
+沒有使用任何 UI 框架，純粹利用 CSS grid、flex 製作前端頁面，並使用媒體查詢做響應式設計。  
+除此之外也修改滾動條、header 背景模糊使其更美觀。  
 
 ## 文件結構
 ```
---
+++ src/Index.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from 'react';
+import IssuesList from './components/IssuesList';
+import api from './api/api';
+import './Index.css';
+
+const CLIENT_ID = '2042d1629d1bc7b1b64f';
+
+interface AccessTokenDataType {
+  access_token?: string;
+  scope?: string;
+  token_type?: string;
+  error?: string;
+  error_description?: string;
+}
+
+function App() {
+  const [loginState, setLoginState] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) {
+      setLoginState(true);
+      return
+    }
+
+    const queryString = window.location.search;
+    const urlParam = new URLSearchParams(queryString);
+    const codeParam: string | null = urlParam.get('code');
+
+    if (!codeParam) return;
+
+    api.getAccessToken(codeParam)
+      .then((data: AccessTokenDataType) => {
+        const token = data?.access_token;
+        if (token) {
+          localStorage.setItem('accessToken', token);
+          setLoginState(true);
+        }
+      })
+  }, [])
+
+  const loginWithGitHub = () => {
+    window.location.assign(`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`);
+  }
+  const logoutWithGithub = () => {
+    localStorage.removeItem('accessToken');
+    setLoginState(false)
+  }
+
+  return (
+    <>
+      <header className='header'>
+        {loginState ?
+          <>
+            <span>Login success!</span>
+            <button onClick={logoutWithGithub}>Logout</button>
+          </>
+          :
+          <button onClick={loginWithGitHub}>Login</button>
+        }
+      </header>
+      {
+        loginState
+          ? <IssuesList />
+          : <p>You need login</p>
+      }
+    </>
+  )
+}
+
+export default App
